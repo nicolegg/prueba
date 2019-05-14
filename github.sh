b34068eb6752f282c41c8f79b207abb90a7d2c65
nicolegg <estefannygarcia@upeu.edu.pe>
2019-05-14 15:00:47 +0000

    comentario

--- github.sh
+++ github.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git add -A
-cd /home/nicole/proyecto && git commit -m 'subio'
+cd /home/nicole/proyecto && git commit -m 'comentario'
 cd /home/nicole/proyecto && git push origin master
 
